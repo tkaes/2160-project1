6eae689821fc8621ca9fdb375864a8f012308468
tkaes <tkuga@uccs.edu>
2025-03-01 16:13:09 -0700

    Creation of read_string and write_string functions
    Created read_string and write_string functions. Stored character array in stack.

--- project1.s
+++ project1.s
@@ -1,4 +1,4 @@
-.globl main 
+.globl main
 .equ STDOUT, 1
 .equ STDIN, 0
 .equ __NR_READ, 63
@@ -7,37 +7,44 @@
 
 .text
 main:
-	# main() prolog
-	addi sp, sp, -104
-	sw ra, 100(sp)
+    # main() prolog
+    addi sp, sp, -104
+    sw ra, 100(sp) # Store ra with space for array
 
-	# main() body
+    # main() body
+    # Call to write_string
+    la a0, prompt               # a0 = prompt address
+    la a1, prompt_end - prompt  # a1 = prompt length
+    jal write_string
 
-	# Write the prompt to the terminal (stdout)
-	li a7, __NR_WRITE
-	li a0, STDOUT
-	la a1, prompt
-	addi  a2, zero, prompt_end - prompt        
-	ecall
+    # Call to read_string
+    jal read_string
 
-	#  Read up to 100 characters from the terminal (stdin)
-	li a7, __NR_READ
-	li a0, STDIN
-	mv a1, sp
-	addi a2, zero, 100
-	ecall
+    # Call to write_string
+    mv a1, a0               # a1 = prompt length
+    lw a0, 0(sp)               # a0 = prompt (array from sp)
+    jal write_string        # Call write_string function
 
-	# Write the just read characters to the terminal (stdout)
-	addi a2, a0, 0
-	li a7, __NR_WRITE
-	li a0, STDOUT
-	mv a1, sp
-	ecall
+    # main() epilog
+    lw ra, 100(sp)
+    addi sp, sp, 104
+    ret
 
-	# main() epilog
-	lw ra, 100(sp)
-	addi sp, sp, 104
-	ret
+write_string:
+    li a7, __NR_WRITE      # a7 = write
+    li a0, STDOUT          # a0 = stdout
+    mv a2, a1       	   # a2 = prompt length
+    mv a1, a0              # a1 = prompt address
+    ecall
+    ret
+
+read_string:
+    li a7, __NR_READ       # a7 = read
+    li a0, STDIN           # a0 = stdin
+    la a1, 0(sp)           # a1 = prompt address
+    addi a2, x0, 100       # a2 = prompt length (max 100)
+    ecall
+    ret
 
 .data
 prompt:   .ascii  "Enter a message: "
