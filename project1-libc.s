36c4b78d3316361bfddf5194ddebb657dbf87064
tkaes <tkuga@uccs.edu>
2025-03-03 11:07:19 -0700

    Create project1-libc.s
    Main skeleton of project1-libc.s

---
+++ project1-libc.s
@@ -0,0 +1,24 @@
+.globl main
+.globl getchar
+.globl putchar
+.globl gets
+.globl puts
+.equ STDOUT, 1
+.equ STDIN, 0
+.equ __NR_READ, 63
+.equ __NR_WRITE, 64
+.equ __NR_EXIT, 93
+
+.text
+main:
+
+getchar:
+
+putchar:
+
+gets:
+
+puts:
+
+.data
+buf: .space 1
