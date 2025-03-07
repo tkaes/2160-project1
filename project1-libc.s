e57dde1f6e68124efa079c11640ac85b8a012e90
tkaes <tkuga@uccs.edu>
2025-03-07 01:07:06 -0700

    Refactor gets
    Refactored gets to use stack and saved registers

--- project1-libc.s
+++ project1-libc.s
@@ -12,48 +12,57 @@
 
 .text
 main:
-    # main body
-    # write prompt
+# main body
+# write prompt
     la a0, prompt           # a0 = ADDR of prompt
     la a1, prompt_end
     call puts
-    # call to gets(buf)
+# call to gets(buf)
     la a0, buf              # a0 = ADDR of buf
     la a1, buf_end
     call gets
-    # call to puts(buf)
+# call to puts(buf)
     la a0, buf              # a0 = ADDR of buf
     la a1, buf_end
     call puts
-    # main epilog
+# main epilog
     j halt
 halt:
     ebreak
     j halt
 
 gets:
-    # a0 = ADDR of prompt
-    li t0, 0                        # t0 = char c (0)
-GETS_LOOP:
-    call getchar
-    bltz a0, GETS_DONE              # if a0 = 0, goto DONE
-    li t1, NEWLINE                  # t1 = newline
-    beq a0, t1, GETS_NEWLINE        # if a0 = t1, goto NEWLINE
-    addi a0, a0, 1                  # increment a0
-    jal GETS_LOOP
-GETS_NEWLINE:
-    mv a0, t1                       # a0 = newline
-    call putchar
-    ret
-GETS_DONE:
-    li a0, -1                       # a0 = -1
-    ret
-
-puts:
-    # puts prolog
+# gets prolog
     addi sp, sp, -4
     sw ra, 0(sp)                    # store ra
-    # puts body
+# gets body
+    mv s0, a0                       # s0 = string pointer (from a0)
+    mv s1, a0                       # s1 = start position in string (aka a0)
+    li s2, buf_end - buf            # s2 = string size
+    li t0, NEWLINE                  # t1 = newline
+    li t1, -1                       # t0 = err
+GETS_LOOP:
+    call getchar
+    sub t0, s1, s0                  # t0 = current string position
+    beq t0, s2, GETS_DONE           # if t0 = string size, goto DONE
+    beq a0, t0, GETS_DONE           # if a0 = newline, goto DONE
+    beq a0, t1, GETS_ERR            # if a0 = -1, goto ERR
+    sb a0, 0(s1)                    # store returned char (a0) at current position in string (a1)
+    addi s1, s1, 1                  # increment string pointer
+    j GETS_LOOP
+GETS_ERR:
+    li a0, 0                        # a0 = null
+GETS_DONE:
+    sb zero, 0(s1)                  # store null terminator at current position in string (a1)
+#gets epilog
+    lw ra, 0(sp)                    # restore stack
+    addi sp, sp, -4
+
+puts:
+# puts prolog
+    addi sp, sp, -4
+    sw ra, 0(sp)                    # store ra
+# puts body
     mv s0, a0                       # s0 = string pointer (from a0)
     mv s1, a1                       # a1 = string end (from a1)
     li t0, -1                       # t0 = err indicator
@@ -72,7 +81,7 @@ PUTS_NEWLINE:
     call putchar
     li a0, 0                        # a0 = success
     jal PUTS_DONE
-    # puts epilog
+# puts epilog
 PUTS_DONE:
     lw ra, 0(sp)                    # restore stack
     addi sp, sp, 4
@@ -91,12 +100,12 @@ EOF_ERR_LOOP:
     li a0, -1                       # a0 = -1
     ret
 
-putchar:  
-    # putchar prolog
+putchar:
+# putchar prolog
     addi sp, sp, -8
     sw ra, 4(sp)                    # store ra
     sw s0, 0(sp)                    # store string pointer from puts
-    # putchar body
+# putchar body
     mv s0, a0                       # s0 = passed char
     li a0, STDOUT                   # a0 = stdout
     li a2, 1                        # a2 = length (1 char)
@@ -107,7 +116,7 @@ putchar:
     ecall
     lbu a0, 0(sp)                   # a0 = passed char
     addi sp, sp, 1                  # restore stack
-    # putchar epilog
+# putchar epilog
     lw s0, 0(sp)                    # load original s0 (string pointer)
     lw ra, 4(sp)                    # load original ra
     addi sp, sp, 8
